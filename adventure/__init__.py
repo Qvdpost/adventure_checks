ee0c452337f65926d89ed03ccf2d0526fa05d4d0
Quinten <qvdpost@gmail.com>
2018-10-18 18:06:19 +0200

    fixed docstrings

--- adventure/__init__.py
+++ adventure/__init__.py
@@ -27,9 +27,7 @@ room_14_description = "You are in a splendid chamber thirty feet high.  The wall
 room_15_description = "You are in a splendid chamber thirty feet high.  The walls are frozen rivers of orange stone.  A narrow canyon and a good passage exit from east and west sides of the chamber. High in the cavern, you see a little bird flying around the rocks.  It takes one look at the black rod and quickly flies out of sight."
 @check50.check()
 def exists():
-    """
-    Checking if all files exist.
-    """
+    """Checking if all files exist."""
     check50.include("data")
     # check50.include("data/CrowtherRooms.txt")
     # check50.include("data/CrowtherItems.txt")
@@ -41,31 +39,23 @@ def exists():
 
 @check50.check(exists)
 def move_once():
-    """
-    Move once.
-    """
+    """Move once."""
     check50.run(run_command).stdout(room_1_description)
 
 @check50.check(exists)
 def move_invalid():
-    """
-    Attempt invalid move.
-    """
+    """Attempt invalid move."""
     check50.run(run_command).stdin("EAST").stdout("Invalid command.")
 
 @check50.check(move_once)
 def move_repeatedly():
-    """
-    Move repeatedly
-    """
+    """Move repeatedly"""
     check50.run(run_command).stdin("WEST").stdout(room_2_description)
     check50.run(run_command).stdin("west").stdin("east").stdout(room_1_name)
 
 @check50.check(move_repeatedly)
 def move_mixed_case():
-    """
-    Move with mixed case command.
-    """
+    """Move with mixed case command."""
     check50.run(run_command).stdin("west").stdout(room_2_description)
     check50.run(run_command).stdin("wESt").stdout(room_2_description)
     check50.run(run_command).stdin("west").stdin("EAST").stdout(room_1_name)
@@ -74,18 +64,14 @@ def move_mixed_case():
 
 @check50.check(move_mixed_case)
 def commands():
-    """
-    Test if program accepts user commands and abbreviations from Synonyms.txt.
-    """
+    """Test if program accepts user commands and abbreviations from Synonyms.txt."""
     check50.run(run_command).stdin("yolo").stdout("Invalid command.").stdin("west").stdout(room_2_description)
     check50.run(run_command).stdin("W").stdout(room_2_description)
     check50.run(run_command).stdin("w").stdout(room_2_description)
 
 @check50.check(commands)
 def helper_commands():
-    """
-    Testing helper commands; HELP, LOOK, QUIT.
-    """
+    """Testing helper commands; HELP, LOOK, QUIT."""
     check50.run(run_command).stdin("HELP").stdout(
         "You can move by typing directions such as EAST/WEST/IN/OUT\n" +
         "QUIT quits the game.\n" +
@@ -102,48 +88,36 @@ def helper_commands():
 
 @check50.check(helper_commands)
 def find_items():
-    """
-    Finds items in rooms.
-    """
+    """Finds items in rooms."""
     check50.run(run_command).stdin("in").stdout(room_3_description + "\nKEYS: a set of keys\nWATER: a bottle of water")
     check50.run(run_command).stdin("in").stdin("out").stdin("in").stdin("look").stdout("KEYS: a set of keys\nWATER: a bottle of water")
 
 @check50.check(find_items)
 def handle_items():
-    """
-    Take and drop items.
-    """
+    """Take and drop items."""
     check50.run(run_command).stdin("in").stdin("TAKE keys").stdout("KEYS taken.")
     check50.run(run_command).stdin("in").stdin("TAKE keys").stdin("out").stdin("DROP keys").stdout("KEYS dropped.").stdin("look").stdout("KEYS: a set of keys\n")
 
 @check50.check(handle_items)
 def handle_invalid_items():
-    """
-    Take and drop nonexistand items.
-    """
+    """Take and drop nonexistand items."""
     check50.run(run_command).stdin("in").stdin("TAKE kes").stdout("No such item.")
     check50.run(run_command).stdin("in").stdin("TAKE keys").stdin("TAKE keys").stdout("No such item.")
     check50.run(run_command).stdin("in").stdin("DROP something").stdout("No such item.")
 
 @check50.check(handle_items)
 def conditional_move():
-    """
-    Testing conditional movements.
-    """
+    """Testing conditional movements."""
     check50.run(run_command).stdin("in").stdin("TAKE keys").stdin("out").stdin("down\ndown\ndown\ndown").stdout("You are in a small chamber beneath a 3x3 steel grate to the surface.  A low crawl over cobbles leads inward to the west.\nLAMP: a brightly shining brass lamp")
     # Check for move with multiple conditions.
     check50.run(run_command).stdin("IN\nTAKE KEYS\nOUT\nDOWN\nDOWN\nDOWN\nDOWN\nTAKE LAMP\nIN\nWEST\nWEST\nWEST\nTAKE BIRD\nWEST\nDOWN\nSOUTH\nTAKE NUGGET\nOUT\nDROP NUGGET\nUP\nEAST\nEAST\nEAST\nTAKE ROD\nWEST\nWEST\nLOOK\n").stdout(room_14_description).stdin("EAST\nDROP BIRD\nWEST\nLOOK\n").stdout(room_15_description)
 
 @check50.check(conditional_move)
 def forced_move():
-    """
-    Testing for forced movements.
-    """
+    """Testing for forced movements."""
     check50.run(run_command).stdin("down\ndown\ndown\ndown").stdout("The grate is locked and you don't have any keys.\nOutside grate")
 
 @check50.check(forced_move)
 def won():
-    """
-    Testing win condition.
-    """
+    """Testing win condition."""
     check50.run(run_command).stdin("IN\nTAKE KEYS\nOUT\nDOWN\nDOWN\nDOWN\nDOWN\nTAKE LAMP\nIN\nWEST\nWEST\nWEST\nTAKE BIRD\nWEST\nDOWN\nSOUTH\nTAKE NUGGET\nOUT\nDROP NUGGET\nUP\nEAST\nEAST\nEAST\nTAKE ROD\nWEST\nWEST\nWEST\nDOWN\nTAKE NUGGET\nWEST\nWAVE\nTAKE DIAMOND\nWEST\nSOUTH\nSOUTH\nEAST\nNORTH\nNORTH\nTAKE CHEST\nOUT\nWEST\nDOWN\nWEST\nDOWN\nNORTH\nEAST\nTAKE COINS\nOUT\nNORTH\nDOWN\nEAST\nDROP LAMP\nDROP BIRD\nDROP NUGGET\nDROP COINS\nNORTH\nTAKE EMERALD\nOUT\nTAKE LAMP\nTAKE BIRD\nTAKE NUGGET\nTAKE COINS\nWEST\nWEST\nWEST\nDOWN\nWATER\nTAKE EGGS\nNORTH\nDOWN\nOUT\nEAST\nEAST\nEAST\nUP\nSOUTH\nSOUTH\nWEST\nWAVE\nWEST\nSOUTH\nNORTH\nNORTH\nEAST\nDOWN\nEAST\nEAST\nXYZZY\nNORTH\n").stdout("You have collected all the treasures and are admitted to the Adventurer's Hall of Fame.  Congratulations!").exit(0)
